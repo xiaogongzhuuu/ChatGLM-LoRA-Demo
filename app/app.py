a1d35630103c89ca6d8d771d55886a9af8cc2411
csyyyyy131 <caosiyuan123456789@163.com>
2025-08-06 17:39:48 +0800

    training

---
+++ app/app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from transformers import AutoTokenizer , AutoModel
+from peft import PeftModel
+import torch
+
+base_model_path="THUDM/chatglm3-6b"
+
+adapter_path="/model"
+
+@st.cache_resource
+def load_model():
+    tokenizer=AutoTokenizer.from_pretrained(base_model_path, trust_remote_code=True)
+    base_model=AutoModel.from_pretrained(base_model_path, trust_remote_code=True)
+    model=PeftModel.from_pretrained(base_model,adapter_path)
+    return tokenizer ,model
+
+tokenizer,model=load_model()
+
+
+
+st.set_page_config(page_title="ChatGLM-LoRA Demo",layout="centered")
+
+st.title("ChatGLM-LoRA 微调模型 Demo")
+
+if"chat_history" not in st.session_state:
+    st.session_state.chat_history=[]
+
+user_input=st.text_input("请输入你的问题",key="input")
+
+
+
--- scripts/train.py
+++ scripts/train.py
@@ -1,12 +1,42 @@
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModel,training_args,trainer
 from peft import get_peft_model, LoraConfig,TaskType
+
+#模型名称
 model_name="THUDM/chatglm3-6b"
 
+#加载tokenizer和模型
 tokenizer=AutoTokenizer.from_pretrained(model_name,trust_remote_code=True)
 model =AutoModel.from_pretrained(model_name,trust_remote_code=True).half().cuda()
 
-
+#应用LoRA配置
 peft_config = LoraConfig(
     task_type=TaskType.CAUSAL_LM,
-    r=8
+    r=8,
+    lora_alpha=32,
+    lora_dropout=0.05,
+    bias="none"
 )
+
+#插入lora配置
+model= get_peft_model(model,peft_config)
+
+#加载训练数据
+from datasets import load_dataset
+
+dataset=load_dataset("json",data_files="data/train.json")["train"]
+
+#拼接prompt
+def format_prompt(example):
+    prompt=example["instruction"]
+    if example.get("input"):
+        prompt+="\n" + example["input"]
+    example["prompt"]=prompt
+    return example
+
+dataset=dataset.map(format_prompt)
+
+
+def tokenize(example):
+    inputs = tokenizer()
+
+    
